74d177883285b49e4b1cbea83747366a5754c7a6
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2018-08-18 14:16:08 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import './logger';
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
 import cluster from 'cluster';
 import os from 'os';
-import path from 'path';
-import fs from 'fs';
-import showdown from 'showdown';
 import route from './routes';
 
 dotenv.config();
@@ -22,22 +20,13 @@ const cpus = os.cpus().length;
 
 const app = express();
 
-const converter = new showdown.Converter({
-  tables: true,
-});
-converter.setFlavor('github');
-
-app.use([bodyParser.json(), bodyParser.urlencoded({ extended: false })]);
+app.use([
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: false }),
+  helmet(),
+]);
 
 app.use('/api', route);
-
-app.get('/api', (req, res) => {
-  const filePath = path.join(__dirname, './docs.md');
-
-  const file = fs.readFileSync(filePath, 'utf8');
-
-  res.send(converter.makeHtml(file));
-});
 
 app.get('/', (req, res) => {
   res.send(`MPA Server.`);
