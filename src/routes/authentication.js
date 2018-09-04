4d1dbad38637dbe03edc46684e108f3b9527127f
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2018-09-04 11:38:24 +0700

    update change password

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import authenticationSuccess from '../jsons/authenticationSuccess';
 import authenticationFail from '../jsons/authenticationFail';
+import submitChangePassword from '../jsons/submitChangePassword';
+import submitChangePasswordError from '../jsons/submitChangePasswordError';
+import submitChangePasswordSuccess from '../jsons/submitChangePasswordSuccess';
 
 const router = Router();
 
@@ -12,6 +15,26 @@ router.post('/submitAuthentication', (req, res) => {
   }
 
   return res.status(200).json(authenticationFail);
+});
+
+router.post('/submitChangePassword', (req, res) => {
+  const {
+    type, userId, oldPassword, newPassword,
+  } = req.query;
+
+  if (userId !== 'mpancs05' || oldPassword !== '1') {
+    return res.json(submitChangePassword);
+  }
+
+  if (oldPassword === '1' && newPassword === oldPassword) {
+    return res.json(submitChangePasswordError);
+  }
+
+  if (type === '2' && userId === 'mpancs05' && oldPassword === '1' && newPassword !== oldPassword) {
+    return res.json(submitChangePasswordSuccess);
+  }
+
+  return res.json(global.errors);
 });
 
 export default router;
