190420ee0b669e3360ac340dd08aa65d2bb2454f
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2018-10-08 15:54:06 +0700

    test new api for news

---
+++ src/routes/__news.js
@@ -0,0 +1,110 @@
+import { Router } from 'express';
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const rootHost = 'https://www.mpa.gov.sg';
+
+const router = Router();
+
+router.get('/news', async (req, res) => {
+  const { page } = req.query;
+
+  const news = [];
+
+  const response = await axios({
+    baseURL: `${rootHost}/web/portal/home/media-centre/news-releases/mpa-news-releases?page=${page || 1}`,
+    method: 'GET',
+  });
+
+  const $ = cheerio.load(response.data);
+
+  try {
+    $('.listings.resize ul li').each((index, li) => {
+      const dateTime = $(li).find('small').text();
+      const title = $(li).find('a').text();
+      const url = `${rootHost}${$(li).find('a').attr('href')}`;
+      const id = url.substring(url.lastIndexOf('/') + 1);
+
+      const newItem = {
+        id,
+        dateTime,
+        title,
+        url,
+        type: 'NEWS',
+      };
+
+      news.push(newItem);
+    });
+
+    return res.json({
+      returnCode: 0,
+      returnMessage: 'success',
+      returnPayload: news,
+    });
+  } catch (error) {
+    return res.json(global.errors);
+  }
+});
+
+router.get('/news_detail', async (req, res) => {
+  const { id } = req.query;
+
+  const payload = {};
+
+  const response = await axios({
+    baseURL: `${rootHost}/web/portal/home/media-centre/news-releases/mpa-news-releases/detail/${id}`,
+    method: 'GET',
+  });
+
+  try {
+    const $ = cheerio.load(response.data);
+
+    const wpthemeInner = $('#layoutContainers .wpthemeInner');
+
+    const title = $(wpthemeInner).children('.pg_title').text();
+
+    const content = $(wpthemeInner).find('.wpthemeControlBody .content_wrapper.resize');
+
+    let date = null;
+
+    $(wpthemeInner).find('.wpthemeControlBody .content_wrapper.resize strong').each((i, strong) => {
+      const dateParsed = Date.parse($(strong).text());
+
+      if (!isNaN(dateParsed)) date = $(strong).text();
+
+      return;
+    });
+
+    const arrayContent = [];
+
+    $(content).find('div, p').each((index, p) => {
+      const pText = $(p).text().trim().replace(/  +/g, ' ') || null;
+
+      const strong = $(p).find('strong').text().trim().replace(/  +/g, ' ') || null;
+
+      const img = $(p).find('img').attr('src') || null;
+
+      const realData = {
+        p: pText,
+        img: (img) ? `${rootHost}${img}` : null,
+        strong,
+      };
+
+      arrayContent.push(realData);
+    });
+
+    payload.title = title;
+    payload.date = date;
+    payload.content = arrayContent;
+
+    return res.json({
+      returnCode: 0,
+      returnMessage: 'success',
+      returnPayload: payload,
+    });
+  } catch (error) {
+    return res.json(global.errors);
+  }
+});
+
+export default router;
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,110 +1,17 @@
 import { Router } from 'express';
-import axios from 'axios';
-import cheerio from 'cheerio';
-
-const rootHost = 'https://www.mpa.gov.sg';
+import news from '../jsons/news';
+import newsById from '../jsons/newsById';
 
 const router = Router();
 
-router.get('/news', async (req, res) => {
-  const { page } = req.query;
-
-  const news = [];
-
-  const response = await axios({
-    baseURL: `${rootHost}/web/portal/home/media-centre/news-releases/mpa-news-releases?page=${page || 1}`,
-    method: 'GET',
-  });
-
-  const $ = cheerio.load(response.data);
-
-  try {
-    $('.listings.resize ul li').each((index, li) => {
-      const dateTime = $(li).find('small').text();
-      const title = $(li).find('a').text();
-      const url = `${rootHost}${$(li).find('a').attr('href')}`;
-      const id = url.substring(url.lastIndexOf('/') + 1);
-
-      const newItem = {
-        id,
-        dateTime,
-        title,
-        url,
-        type: 'NEWS',
-      };
-
-      news.push(newItem);
-    });
-
-    return res.json({
-      returnCode: 0,
-      returnMessage: 'success',
-      returnPayload: news,
-    });
-  } catch (error) {
-    return res.json(global.errors);
-  }
-});
-
-router.get('/news_detail', async (req, res) => {
+router.get('/news', (req, res) => {
   const { id } = req.query;
 
-  const payload = {};
-
-  const response = await axios({
-    baseURL: `${rootHost}/web/portal/home/media-centre/news-releases/mpa-news-releases/detail/${id}`,
-    method: 'GET',
-  });
-
-  try {
-    const $ = cheerio.load(response.data);
-
-    const wpthemeInner = $('#layoutContainers .wpthemeInner');
-
-    const title = $(wpthemeInner).children('.pg_title').text();
-
-    const content = $(wpthemeInner).find('.wpthemeControlBody .content_wrapper.resize');
-
-    let date = null;
-
-    $(wpthemeInner).find('.wpthemeControlBody .content_wrapper.resize strong').each((i, strong) => {
-      const dateParsed = Date.parse($(strong).text());
-
-      if (!isNaN(dateParsed)) date = $(strong).text();
-
-      return;
-    });
-
-    const arrayContent = [];
-
-    $(content).find('div, p').each((index, p) => {
-      const pText = $(p).text().trim().replace(/  +/g, ' ') || null;
-
-      const strong = $(p).find('strong').text().trim().replace(/  +/g, ' ') || null;
-
-      const img = $(p).find('img').attr('src') || null;
-
-      const realData = {
-        p: pText,
-        img: (img) ? `${rootHost}${img}` : null,
-        strong,
-      };
-
-      arrayContent.push(realData);
-    });
-
-    payload.title = title;
-    payload.date = date;
-    payload.content = arrayContent;
-
-    return res.json({
-      returnCode: 0,
-      returnMessage: 'success',
-      returnPayload: payload,
-    });
-  } catch (error) {
-    return res.json(global.errors);
+  if (id) {
+    return res.status(200).json(newsById);
   }
+
+  return res.status(200).json(news);
 });
 
 export default router;
