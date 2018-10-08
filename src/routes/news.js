94aef90fafad03e571e7596dcd66ea2ccedfa956
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2018-10-09 00:26:09 +0700

    done all news, circulars & notices

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,23 @@
 import { Router } from 'express';
 import news from '../jsons/news';
 import newsById from '../jsons/newsById';
+import articles from '../jsons/articles';
+import circularsShipping from '../jsons/circularsShipping';
+import circularsShippingById from '../jsons/circularsShippingById';
+import circularsPortMarine from '../jsons/circularsPortMarine';
+import circularsPortMarineById from '../jsons/circularsPortMarineById';
+import noticesPortMarine from '../jsons/noticesPortMarine';
+import noticesPortMarineById from '../jsons/noticesPortMarineById';
+import noticesMaritimeSecurity from '../jsons/noticesMaritimeSecurity';
+import noticesMaritimeSecurityById from '../jsons/noticesMaritimeSecurityById';
+import noticesSingaporeanNoticesToMariners from '../jsons/noticesSingaporeanNoticesToMariners';
+import noticesSingaporeanNoticesToMarinersById from '../jsons/noticesSingaporeanNoticesToMarinersById';
 
 const router = Router();
+
+router.get('/articles', (req, res) => {
+  res.status(200).json(articles);
+});
 
 router.get('/news', (req, res) => {
   const { id } = req.query;
@@ -12,6 +27,49 @@ router.get('/news', (req, res) => {
   }
 
   return res.status(200).json(news);
+});
+
+router.get('/circulars', (req, res) => {
+  const { category, id } = req.query;
+
+  switch (category) {
+    case 'Shipping Circulars': {
+      if (id) return res.status(200).json(circularsShippingById);
+
+      return res.status(200).json(circularsShipping);
+    }
+    case 'Port Marine Circulars': {
+      if (id) return res.status(200).json(circularsPortMarineById);
+
+      return res.status(200).json(circularsPortMarine);
+    }
+    default:
+      return res.status(400).json(global.errors);
+  }
+});
+
+router.get('/notices', (req, res) => {
+  const { category, id } = req.query;
+
+  switch (category) {
+    case 'Port Marine Notices': {
+      if (id) return res.status(200).json(noticesPortMarineById);
+
+      return res.status(200).json(noticesPortMarine);
+    }
+    case 'Maritime Security Notices': {
+      if (id) return res.status(200).json(noticesMaritimeSecurityById);
+
+      return res.status(200).json(noticesMaritimeSecurity);
+    }
+    case 'Singaporean Notices to Mariners': {
+      if (id) return res.status(200).json(noticesSingaporeanNoticesToMarinersById);
+
+      return res.status(200).json(noticesSingaporeanNoticesToMariners);
+    }
+    default:
+      return res.status(400).json(global.errors);
+  }
 });
 
 export default router;
